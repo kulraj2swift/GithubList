9f625103ffbf12a5246e00d683714ce1cb639c4b
kulraj-singh <koolraj003@gmail.com>
2025-04-19 03:46:08 +0530

    few files added

---
+++ GithubList/GithubList/ApiManager.swift
@@ -0,0 +1,28 @@
+//
+//  ApiManager.swift
+//  GithubList
+//
+//  Created by kulraj singh on 19/04/25.
+//
+
+import Foundation
+import Alamofire
+
+struct Urls {
+    static let baseUrl = "https://api.github.com/"
+    static let searchEndpoint = "search/users"
+}
+
+class ApiManager {
+    
+    static let shared = ApiManager()
+    
+    private init() {}
+    
+    func getHttpHeaders() -> HTTPHeaders {
+        var headers = HTTPHeaders()
+        headers.add(name: "X-GitHub-Api-Version", value: "2022-11-28")
+        headers.add(name: "accept", value: "application/vnd.github+raw")
+        return headers
+    }
+}
---
+++ GithubList/GithubList/Controllers/Users/UserListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserListViewModel.swift
+//  GithubList
+//
+//  Created by kulraj singh on 19/04/25.
+//
+
+import Foundation
